d8d32db4c1b374278cd6a5f3e5a4b5d71190a01a
harman <harmandeeps008@gmail.com>
2021-08-16 10:49:11 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbDQUUQxGWozf84aTb8djvJv03BqMz9vE",
-    authDomain: "personal-blog-950a5.firebaseapp.com",
-    projectId: "personal-blog-950a5",
-    storageBucket: "personal-blog-950a5.appspot.com",
-    messagingSenderId: "773878057382",
-    appId: "1:773878057382:web:af4ce5e9465b022ab5a767"
+    
   };
 
 firebase.initializeApp(firebaseConfig);
